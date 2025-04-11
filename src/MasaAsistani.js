748f6b2e540b6d197ce211d80abc682526faf142
Ali Burak <aliburak9627@gmail.com>
2025-04-11 17:00:00 +0300

    Backend bağlantısı düzeltildi

--- src/MasaAsistani.js
+++ src/MasaAsistani.js
@@ -38,7 +38,7 @@ function MasaAsistani() {
 
   const sendToBackend = async (spokenText) => {
     try {
-      const response = await fetch("http://localhost:8000/neso", {
+      const response = await fetch("https://neso-backend-clean.onrender.com/sesli-siparis", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: spokenText, masa: masaId }),
